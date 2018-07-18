dec21dc24b7987c4b39fa9bd45e91fd174954d48
570gina <570gina@gmail.com>
2018-07-18 13:16:00 +0800

    718

---
+++ src/bbdp/doctor/controller/QuestionImageServletListener.java
@@ -0,0 +1,28 @@
+package bbdp.doctor.controller;
+
+import java.io.File;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class QuestionImageServletListener implements ServletContextListener{
+	@Override
+	public void contextInitialized(ServletContextEvent event) {
+		ServletContext servletContext = event.getServletContext();
+		String imagePath = servletContext.getRealPath("/");
+		String questionImagePath = imagePath + "QuestionImage";
+		File path = new File(questionImagePath);
+
+		if (!path.exists()) {
+			path.mkdirs();
+		}
+
+		System.out.println("QuestionImage is located here: " + questionImagePath);
+	}
+	
+	@Override
+	public void contextDestroyed(ServletContextEvent arg0) {
+	
+	}
+}
